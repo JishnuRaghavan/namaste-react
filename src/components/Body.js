5416922163aa4d26d965b98c7655c362a5199cfc
JishnuRaghavan <jishnu18js@gmail.com>
2024-11-03 10:27:34 +0530

    changed css to tailwind

--- src/components/Body.js
+++ src/components/Body.js
@@ -29,21 +29,23 @@ const Body = ()=>{
   if(onlineStatus == false)return<h1>looks like you are offline!!! please check your internet connection.</h1>
     return filteredRestaurents.length === 0 ? <Shimmer /> : (
         <div className="body">
-            <div className="filter">
-              <div className="search">
-                <input type="text" value={searchText} onChange={(e)=>setSearchText(e.target.value)} className="search-box" />
-                <button onClick={()=>{
+            <div className="flex items-center">
+              <div className="search m-4 p-4">
+                <input type="text" value={searchText} onChange={(e)=>setSearchText(e.target.value)} className="border border-solid border-black" />
+                <button className="px-4 bg-green-100 mx-4 rounded-lg hover:shadow-lg hover:bg-green-200" onClick={()=>{
                   const searchedRes = listOfRestaurant.filter(res=>res.info.name.toLowerCase().includes(searchText.toLowerCase()));
                   setFilteredRestaurents(searchedRes);
                 }}>search</button>
               </div>
-              <button className="filter-btn" onClick={()=>{
+              <div className="m-4 p-4 flex items-center">
+              <button className="px-4 py-2 bg-gray-100 rounded-lg hover:bg-gray-200 hover:shadow-lg" onClick={()=>{
                 const filteredList  = listOfRestaurant.filter(res=>res.info.avgRating > 4.3);
                 setFilteredRestaurents(filteredList);
                 console.log(filteredList);
               }}>Top Rated Restaurants</button>
+              </div>
             </div>
-            <div className="res-container">
+            <div className="flex flex-wrap justify-between">
               {
                 filteredRestaurents.map((restaurant) =>(
                   <Link key={restaurant.info.id} to={'/restaurants/'+restaurant.info.id}><RestaurantCard resData={restaurant} /></Link>
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,18 +8,18 @@ const Header    =   ()=>{
     const [btnName,setBtnName]   =   useState('Login')
     const onlineStatus  = useOnlineStatus();
     return (
-        <div className="header">
+        <div className="flex justify-between bg-pink-100 shadow-lg my-2">
             <div className="logo-container">
-                <img className="logo" src={LOGO_URL}></img>
+                <img className="w-24" src={LOGO_URL}></img>
             </div>
             <div className="nav-items">
-                <ul>
-                   <li>Online status: {onlineStatus?"✔":"🔴"}</li>                  
-                   <li><Link to='/'>Home</Link></li>
-                   <li><Link to='/about'>About Us</Link></li>
-                   <li><Link to='/contact'>Contact Us</Link></li>
-                   <li><Link to='/grocery'>Grocery</Link></li>
-                   <li><Link>Cart</Link></li>
+                <ul className="flex p-4 m-4 items-center">
+                   <li className="px-4">Online status: {onlineStatus?"✔":"🔴"}</li>                  
+                   <li className="px-4"><Link to='/'>Home</Link></li>
+                   <li className="px-4"><Link to='/about'>About Us</Link></li>
+                   <li className="px-4"><Link to='/contact'>Contact Us</Link></li>
+                   <li className="px-4"><Link to='/grocery'>Grocery</Link></li>
+                   <li className="px-4"><Link>Cart</Link></li>
                    <button onClick={()=>{
                     btnName == "Login"? setBtnName('Logout'):setBtnName('Login')
                    }} className="login">{btnName}</button>
